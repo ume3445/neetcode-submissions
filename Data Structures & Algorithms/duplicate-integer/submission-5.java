1dd033ac840dd974cd538dd16b1cd72c9ad72fad
ume3445 <umerhammad010@gmail.com>
2026-05-21 20:30:19 -0400

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,25 @@
+/*
+I : array of integers
+O : Boolean
+P : Create a new hashset. Iterate through the array. Insert every element in the hashset.
+    Before inserting check if the number is already in the array. If yes return true. 
+    if not return false.
+*/
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet <Integer> set = new HashSet<>();
+        if (nums.length == 0) {
+            return false;
+        }
+        set.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}

7390a5b957c30520b8a84cc1cdd64e2bfa46cf37
Umer Hammad <umerhammad010@gmail.com>
2026-06-11 20:03:51 -0400

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int right = 0;
+        int left = numbers.length -1;
+        int[] answer = new int[2];
+        while (right < left) {
+            while (right < left) {
+                if ((numbers[right] + numbers[left]) == target) {
+                    answer[0] = (right +1);
+                    answer[1] = (left +1);
+                    return answer;
+                } else {
+                left--;
+                }
+            }
+            right++;
+            left = numbers.length - 1;
+        }
+
+        return answer;
+    }
+}

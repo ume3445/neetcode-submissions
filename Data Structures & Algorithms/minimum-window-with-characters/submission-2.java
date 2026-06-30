cc9e4f69aefd7627b6661bbcff33f6dd7bf891fd
Umer Hammad <umerhammad010@gmail.com>
2026-06-30 01:38:12 -0400

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.length() > s.length()) {
+            return "";
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int i = 0;
+        int windowSize = Integer.MAX_VALUE;
+        int need = t.length();
+        int r = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            if (map.getOrDefault(c, 0) > 0) {
+                need--;
+            } 
+            map.put(c, map.getOrDefault(c, 0) - 1);
+
+            while (need == 0) {
+                int temp = (j - i) + 1;
+                if (temp < windowSize) {
+                    windowSize = temp;
+                    r = i;
+                }
+                char leftChar = s.charAt(i);
+                map.put(leftChar, map.getOrDefault(leftChar, 0 ) + 1);
+                if (map.get(leftChar) > 0) {
+                    need++;
+                }
+                i++;
+            }
+
+            continue;
+        }
+
+        if (windowSize == Integer.MAX_VALUE) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder(windowSize);
+        for (int x = r; x <  r + windowSize; x++) {
+            sb.append(s.charAt(x));
+        }
+        return sb.toString();
+    }
+}
+ 

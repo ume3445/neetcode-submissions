824426665c3f7be95039988f5d4608b3c9cbb43f
Umer Hammad <umerhammad010@gmail.com>
2026-07-03 00:35:34 -0400

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (stack.size() > 0) {
+                if (c == ']') {
+                    if (stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (c == ')') {
+                    if (stack.peek() == '(') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (c == '}') {
+                    if (stack.peek() == '{') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
